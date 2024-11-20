b63ffc9badf1c317e778a716aa9c9a9017ac42a4
Anny Espinel <annyespinel@Annys-MacBook-Pro.local>
2024-11-20 00:56:07 -0500

    Primer commit - Proyecto subido a GitHub

---
+++ script.js
@@ -0,0 +1,7 @@
+function verMas() {
+    alert('¡Gracias por tu interés! Aquí se podrá añadir más información sobre mi perfil profesional.');
+}
+
+function descargarCV() {
+    window.location.href = 'CV_JuanPerez.pdf'; // Asegúrate de tener el archivo CV_JuanPerez.pdf en la misma carpeta
+}
